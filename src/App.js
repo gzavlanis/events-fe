b74a8868f4e6c1ce78520294ed710779cf5a9f6a
gzavlanis <gzavlanis@gmail.com>
2022-12-27 21:28:47 +0200

    add contact page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Dashboard from './components/Dashboard';
+import Contact from './components/ContactPage';
 
 export default class App extends React.Component{
   render(){
@@ -10,6 +11,7 @@ export default class App extends React.Component{
         <Routes>
             <Route exact path= "/" element= {<Home/>}/>
             <Route exact path= "/dashboard" element= {<Dashboard/>}/>
+            <Route exact path= "/contact" element= {<Contact/>}/>
         </Routes>
       </BrowserRouter>
     );
---
+++ src/components/ContactPage.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Button, Form, FormGroup, Input, Label, FormText, Row, Col } from "reactstrap";
+import Sidebar from "./Sidebar";
+
+export default class Contact extends React.Component{
+    render(){
+        return(
+            <Row style= {{backgroundColor: "lightgray", height: "100vh"}}>
+                <Col className= "col-3">
+                    <Sidebar/>
+                </Col>
+                <Col className= "col-9">
+                    <h3 className= "text-center">Contact with the programmers</h3>
+                    <Form>
+                        <FormGroup>
+                            <Label for= "name">Name</Label>
+                            <Input className= "shadow" type= "text" name= "name" id= "name" placeholder= "Enter your name"/>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for= "email">Email</Label>
+                            <Input className= "shadow" type= "email" name= "email" id= "email" placeholder= "Enter your Email"/>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for= "message">Message</Label>
+                            <Input className= "shadow" type= "textarea" name= "message" id= "message" placeholder= "Write your Message"/>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for= "file">Add files to help us uderstand</Label>
+                            <Input className= "shadow" type= "file" name= "file" id= "file"/>
+                            <FormText color= "dark">You can upload image files with shots from your screen, to help understand your issue.</FormText>
+                        </FormGroup>
+                        <Button className= "shadow">
+                            <i className= "fa fa-paper-plane"></i>
+                            &nbsp;Send
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        );
+    }
+}
