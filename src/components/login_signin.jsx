2761e015a5622be06cd3da633c3c9c826f5862a6
gzavlanis <gzavlanis@gmail.com>
2023-01-08 15:12:29 +0200

    update

--- src/components/login_signin.jsx
+++ src/components/login_signin.jsx
@@ -1,24 +1,67 @@
-import React from "./react";
+import React, { useState } from "./react";
 import { Button, Container, Form, FormGroup, Input, Label } from "reactstrap";
 
 export default function Login(props){
+    let [authMode, setAuthMode]= useState("signin");
+    const changeAuthMode= () => {
+        setAuthMode(authMode === "signin" ? "signup" : "signin")
+    }
+
+    if (authMode === "signin"){
+        return(
+            <Container className= "Auth-form-container">
+                <Form className= "Auth-form">
+                    <div className= "Auth-form-content">
+                        <h3 className= "Auth-form-title">Sign In</h3>
+                        <div className= "text-center">
+                            Not registered yet?{" "}
+                            <span className= "link-primary" onClick= {changeAuthMode}>Sign up</span>
+                        </div>
+                        <FormGroup className= "mt-3">
+                            <Label>Email address</Label>
+                            <Input type= "email" className= "form-control mt-1" placeholder= "Enter email"/>
+                        </FormGroup>
+                        <FormGroup className= "mt-3">
+                            <Label>Password</Label>
+                            <Input type= "password" className= "form-control mt-1" placeholder= "Enter password"/>
+                        </FormGroup>
+                        <div className= "d-grid gap-2 mt-3">
+                            <Button type= "submit" className= "btn btn-primary">Submit</Button>
+                        </div>
+                        <p className= "forgot-password text-right mt-2">
+                            Forgot <a href= "#">password?</a>
+                        </p>
+                    </div>
+                </Form>
+            </Container>
+        );
+    }
+
     return(
         <Container className= "Auth-form-container">
             <Form className= "Auth-form">
                 <div className= "Auth-form-content">
                     <h3 className= "Auth-form-title">Sign In</h3>
+                    <div className= "text-center">
+                        Already Registered?{" "}
+                        <span className= "link-primary" onClick= {changeAuthMode}>Sign In</span>
+                    </div>
                     <FormGroup className= "mt-3">
-                        <Label>Email address</Label>
-                        <Input type= "email" className= "form-control mt-1" placeholder= "Enter email"/>
+                        <Label>Full name</Label>
+                        <Input type= "text" className= "form-control mt-1" placeholder= "e.g. John Doe"/>
+                    </FormGroup>
+                    <FormGroup className= "mt-3">
+                        <Label>Email</Label>
+                        <Input type= "email" className= "form-control mt-1" placeholder= "Email address"/>
                     </FormGroup>
                     <FormGroup className= "mt-3">
                         <Label>Password</Label>
-                        <Input type= "password" className= "form-control mt-1" placeholder= "Enter password"/>
+                        <Input type= "password" className= "form-control mt-1" placeholder= "Password"/>
                     </FormGroup>
                     <div className= "d-grid gap-2 mt-3">
                         <Button type= "submit" className= "btn btn-primary">Submit</Button>
                     </div>
-                    <p className= "forgot-password text-right mt-2">
+                    <p className= "text-center mt-2">
                         Forgot <a href= "#">password?</a>
                     </p>
                 </div>
